c66b0d22617008cf28fa9260d31fac7ab7b0d9cb
PJ <petar_jovic@hotmail.com>
2025-12-19 13:22:25 -0500

    bugfix for characters sometimes disappearing

--- whos_that_front/src/components/game/GameBoard.tsx
+++ whos_that_front/src/components/game/GameBoard.tsx
@@ -1,4 +1,4 @@
-import type { JSX } from "react";
+import { useState, type JSX } from "react";
 
 interface gridColsTailwind {
     [key: number]: string;
@@ -33,7 +33,9 @@ const GameBoard = ({
     let numGridCols = Math.ceil(Math.sqrt(cardList.length)) + 4;
     if (numGridCols > 12) numGridCols = 12;
 
-    const lastRow = cardList.splice(cardList.length - (cardList.length % numGridCols));
+    const remainder = cardList.length % numGridCols;
+    const mainCards = remainder === 0 ? cardList : cardList.slice(0, -remainder);
+    const lastRow = remainder === 0 ? [] : cardList.slice(-remainder);
 
     return (
         <>
@@ -44,7 +46,7 @@ const GameBoard = ({
                     id="gameboard"
                     className={`mx-auto mb-2.5 max-lg:px-1 lg:px-2.5 2xl:grid 2xl:px-6 ${GridColsClasses[numGridCols]} 2xl:gap-y-4.5 w-full max-2xl:flex max-2xl:flex-wrap max-2xl:items-center max-2xl:justify-around max-md:gap-2 md:max-2xl:gap-2 2xl:auto-cols-min 2xl:place-items-center 2xl:justify-center 2xl:gap-x-0`}
                 >
-                    {cardList}
+                    {mainCards}
                 </div>
                 {/* Last Row of Cards, always flexbox, looks better*/}
                 <div className="mb-2.5 flex w-full flex-wrap justify-evenly px-10 max-2xl:px-4 max-sm:gap-x-8 max-sm:gap-y-3">
